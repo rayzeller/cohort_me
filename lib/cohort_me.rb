f0bcd8b283b7b2babcddebf2f7705540118f1441
rayzeller@gmail.com <rayzeller@gmail.com>
2013-07-18 16:00:56 -0700

    optimize queries

--- lib/cohort_me.rb
+++ lib/cohort_me.rb
@@ -26,23 +26,24 @@ module CohortMe
     start_from = nil
     time_conversion = nil
     cohort_label = nil
+    day_offset = (options[:day_offset] || 0).to_i
 
     if interval_name == "weeks"
-      start_from = start_from_interval.weeks.ago.beginning_of_week
+      start_from = start_from_interval.weeks.ago.beginning_of_week + day_offset.days
       time_conversion = 604800
     elsif interval_name == "days"
       start_from = start_from_interval.days.ago.beginning_of_day
       time_conversion = 86400
     elsif interval_name == "months"
-      start_from = start_from_interval.months.ago.beginning_of_month
+      start_from = start_from_interval.months.ago.beginning_of_month + day_offset.days
       time_conversion = 1.month.seconds
     end
 
-    activation_conversion = self.convert_to_cohort_date_in_postgres("#{activation_table_name}.#{activation_time_column}", interval_name)
-    activity_conversion = self.convert_to_cohort_date_in_postgres("#{activity_table_name}.created_at", interval_name)
+    activation_conversion = self.convert_to_cohort_date_in_postgres("#{activation_table_name}.#{activation_time_column}", start_from, time_conversion)
+    activity_conversion = self.convert_to_cohort_date_in_postgres("#{activity_table_name}.created_at", start_from, time_conversion)
 
-    cohort_query = activation_class.select("#{activation_table_name}.#{activation_user_id}, MIN(#{activation_conversion}) as cohort_date, MIN(#{activation_table_name}.#{activation_time_column}) as cohort_date_exact").where("#{activation_table_name}.#{activation_time_column} > ?", start_from)
-    activated_query = activation_class.select("count(*) number_activated, #{activation_conversion} cohort_date")
+    cohort_query = activation_class.select("#{activation_table_name}.#{activation_user_id}, MIN(#{activation_conversion}) as cohort_date, MIN(#{activation_table_name}.#{activation_time_column}) as cohort_date_exact").where("#{activation_table_name}.#{activation_time_column} >= ?", start_from).where("#{activation_conversion} < ?", start_from + (start_from_interval * time_conversion).seconds)
+    activated_query = activation_class.select("count(*) number_activated, #{activation_conversion} cohort_date").where("#{activation_table_name}.#{activation_time_column} >= ?", start_from).where("#{activation_conversion} < ?", start_from + (start_from_interval * time_conversion).seconds)
 
     if activation_conditions
       activated_query = activated_query.where(activation_conditions)
@@ -58,14 +59,14 @@ module CohortMe
     if %(mysql mysql2).include?(ActiveRecord::Base.connection.instance_values["config"][:adapter])
       select_sql = "#{activity_table_name}.#{activity_user_id}, #{activity_table_name}.created_at, cohort_date, FLOOR(TIMEDIFF(#{activity_table_name}.created_at, cohort_date)/#{time_conversion}) as periods_out"
     elsif ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
-      select_sql = "count(*) count, cohort_date, FLOOR(extract(epoch from (#{activity_table_name}.created_at - cohort_date_exact))/#{time_conversion}) as periods_out"
+      select_sql = "count(distinct #{activity_table_name}.#{activity_user_id}) count, cohort_date, FLOOR(extract(epoch from (#{activity_table_name}.created_at - cohort_date_exact))/#{time_conversion}) as periods_out"
       select_summary_sql = "#{summary_column} summary_value, cohort_date, FLOOR(extract(epoch from (#{activity_table_name}.created_at - cohort_date_exact))/#{time_conversion}) as periods_out"
     else
       raise "database not supported"
     end
 
-    summary_query = activity_class.where("#{activity_table_name}.created_at > ?", start_from).select(select_summary_sql).joins("JOIN (" + cohort_query.to_sql + ") AS cohorts ON #{activity_table_name}.#{activity_user_id} = cohorts.#{activation_user_id}").group("cohort_date,periods_out")
-    data = activity_class.where("#{activity_table_name}.created_at > ?", start_from).select(select_sql).joins("JOIN (" + cohort_query.to_sql + ") AS cohorts ON #{activity_table_name}.#{activity_user_id} = cohorts.#{activation_user_id}").group("cohort_date,periods_out")
+    summary_query = activity_class.where("#{activity_table_name}.created_at >= ?", start_from).select(select_summary_sql).joins("JOIN (" + cohort_query.to_sql + ") AS cohorts ON #{activity_table_name}.#{activity_user_id} = cohorts.#{activation_user_id}").where("orders.created_at < cohort_date + #{time_conversion} * #{start_from_interval} * INTERVAL '1 SECOND'", time_conversion).where('cohort_date < ?',start_from + (start_from_interval*time_conversion).seconds).where('cohort_date <= ?',start_from + (start_from_interval*time_conversion).seconds).group("cohort_date,periods_out")
+    data = activity_class.where("#{activity_table_name}.created_at >= ?", start_from).select(select_sql).joins("JOIN (" + cohort_query.to_sql + ") AS cohorts ON #{activity_table_name}.#{activity_user_id} = cohorts.#{activation_user_id}").where('cohort_date <= ?',start_from + (start_from_interval*time_conversion).seconds).where("orders.created_at < cohort_date + #{time_conversion} * #{start_from_interval} * INTERVAL '1 SECOND'").group("cohort_date,periods_out")
     
     if activity_conditions
       summary_query = summary_query.where(activity_conditions)
@@ -73,10 +74,10 @@ module CohortMe
     end
     
     # unique_data = data.all.uniq{|d| [d.send(activity_user_id), d.cohort_date, d.periods_out] }
-    activated_data = Hash[activated_query.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name)}]
-    summary_data = Hash[summary_query.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name)}]
+    activated_data = Hash[activated_query.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name, day_offset)}]
+    summary_data = Hash[summary_query.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name, day_offset)}]
 
-    analysis = data.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name)}
+    analysis = data.group_by{|d| convert_to_cohort_date(Time.parse(d.cohort_date.to_s), interval_name, day_offset)}
 
     cohort_hash =  Hash[analysis.sort_by { |cohort, data| cohort }]
     table = {}
@@ -88,7 +89,7 @@ module CohortMe
 
       table[r[0]] = {}
 
-      cohort_hash.size.times{|i| periods << r[1].select{|d| d.periods_out.to_i == i}.count  if r[1]} 
+      cohort_hash.size.times{|i| periods << r[1].select{|d| d.periods_out.to_i == i}[0].count  if r[1].select{|d| d.periods_out.to_i == i}[0] } 
       cohort_hash.size.times{|i| summaries << summary_data[r[0]].select{|d| d.periods_out.to_i == i}[0].summary_value  if summary_data[r[0]].select{|d| d.periods_out.to_i == i}[0]} 
 
       table[r[0]][:start] = activated_data[r[0]][0].number_activated
@@ -105,20 +106,32 @@ module CohortMe
 
   end
 
-  def self.convert_to_cohort_date_in_postgres(column_name, interval)
-    "date_trunc('#{interval.singularize}', #{column_name} at time zone 'UTC' at time zone 'US/Pacific')"
+  def self.convert_to_cohort_date_in_postgres(column_name, start_time, interval_in_seconds)
+    %Q{
+      timestamp '#{start_time}'  at time zone 'US/Pacific' + 
+      FLOOR(  
+        extract(
+          epoch from (#{column_name} - timestamp '#{start_time}'  at time zone 'US/Pacific')
+          )/
+          (
+            #{interval_in_seconds}
+          )
+        )
+       * INTERVAL '#{interval_in_seconds} SECOND'
+    }
+    # "date_trunc('#{interval.singularize}', #{column_name} at time zone 'UTC' at time zone 'US/Pacific' - interval '#{day_offset} DAY') + interval '#{day_offset} DAY'"
   end
 
-  def self.convert_to_cohort_date(datetime, interval)
-    if interval == "weeks"
-      return datetime.at_beginning_of_week.to_date
+  def self.convert_to_cohort_date(datetime, interval, day_offset = 0)
+    # if interval == "weeks"
+    #   return datetime.at_beginning_of_week.to_date + day_offset.days
       
-    elsif interval == "days"
+    # elsif interval == "days"
       return Date.parse(datetime.strftime("%Y-%m-%d"))
 
-    elsif interval == "months"
-      return Date.parse(datetime.strftime("%Y-%m-1"))
-    end
+    # elsif interval == "months"
+      # return Date.parse(datetime.strftime("%Y-%m-1")) + day_offset.days
+    # end
   end
 
 
